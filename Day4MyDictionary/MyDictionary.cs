fc6e04360bbc7b4396d6f1b2fa8e26df75cd445e
atillaarin.cokmez <atillaarin.cokmez@COKMEZATILLA>
2021-01-28 22:44:25 +0300

    Add project files.

---
+++ Day4MyDictionary/MyDictionary.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4MyDictionary
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        Tkey[] Tkeys;
+        Tvalue[] Tvalues;
+        Tkey[] TkeysTemp;
+        Tvalue[] TvaluesTemp;
+
+        public MyDictionary()
+        {
+            Tkeys = new Tkey[0];
+            Tvalues = new Tvalue[0];
+          
+        }
+
+        public void Add(Tkey key,Tvalue value)
+        {
+            TkeysTemp = Tkeys;
+            TvaluesTemp = Tvalues;
+
+            Tkeys = new Tkey[Tkeys.Length + 1];
+            
+            for (int i = 0; i < Tkeys.Length; i++)
+            {
+                Tkeys[i] = TkeysTemp[i];
+
+            }
+            Tkeys[Tkeys.Length - 1] = key;
+
+            Tvalues = new Tvalue[Tvalues.Length + 1];
+
+            for (int i = 0; i < Tvalues.Length; i++)
+            {
+                Tvalues[i] = TvaluesTemp[i];
+            }
+            Tvalues[Tvalues.Length - 1] = value;
+
+        }
+        public int Length
+        {
+            get { return Tkeys.Length; }
+        }
+
+        public Tkey[] Items
+        {
+            get { return Tkeys; }
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+            
+
+}
+
+
+
---
+++ Day4MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day4MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> selam = new MyDictionary<int, string> { };
+            selam.Add(14, "meraba");
+            selam.Add(15, "heyo");
+
+            for (int i = 0; i < selam.Length; i++)
+            {
+                Console.WriteLine(selam.Items[0]);
+            }
+            
+        }
+    }
+}
